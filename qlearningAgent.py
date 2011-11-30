cc295b2076abf656835bde1d8c8a38b27f72bb57
Taylor Savage <git@taylorsavage.com>
2011-11-29 23:42:32 -0800

    Added qlearningAgent.py to hold our qlearning agent any any subclasses.

---
+++ qlearningAgent.py
@@ -0,0 +1,92 @@
+from captureAgents import CaptureAgent
+from baselineAgents import ReflexCaptureAgent
+
+class QLearningAgent(ReflexCaptureAgent):
+    def __init__(self, index):
+        CaptureAgent.__init__(self, index)
+        self.firstTurnComplete = False
+        self.startingFood = 0
+        self.theirStartingFood = 0
+        self.discount = .9
+
+    def getFeatures(self, gameState, action):
+        """
+        Returns a counter of features for the state
+        """
+        features = self.getMutationFeatures(gameState, action)
+        return features
+
+    def getWeights(self, gameState, action):
+        """
+        Normally, weights do not depend on the gamestate.    They can be either
+        a counter or a dictionary.
+        """
+        return {'successorScore': 1.0}
+    
+    def getReward(self, state):
+        return 0
+        
+    def getValue(self, state);
+        return self.evaluate(state, self.chooseAction(state))
+        
+    def update(self, state, action, nextState, reward):
+    """
+       Should update your weights based on transition
+    """
+        "*** YOUR CODE HERE ***"
+        features = self.getFeatures(state, action)
+                
+        correction = (self.getReward(state) + self.discount * self.getValue(nextState)) - self.evaluate(state, action)
+        for feature in features.keys():
+            self.weights[feature] += correction * self.getFeatures(state, action)[feature]
+
+    """
+    Features (not the best features) which have learned weight values stored.
+    """
+    def getMutationFeatures(self, gameState, action):
+        features = util.Counter()
+        successor = self.getSuccessor(gameState, action)
+        position = self.getPosition(gameState)
+
+        distances = 0.0
+        for tpos in self.getTeamPositions(successor):
+            distances = distances + abs(tpos[0] - position[0])
+        features['xRelativeToFriends'] = distances
+        
+        enemyX = 0.0
+        for epos in self.getOpponentPositions(successor):
+            if epos is not None:
+                enemyX = enemyX + epos[0]
+        features['avgEnemyX'] = distances
+        
+        foodLeft = len(self.getFoodYouAreDefending(successor).asList())
+        features['percentOurFoodLeft'] = foodLeft / self.startingFood
+        
+        foodLeft = len(self.getFood(successor).asList())
+        features['percentTheirFoodLeft'] = foodLeft / self.theirStartingFood
+        
+        features['IAmAScaredGhost'] = 1.0 if self.isPacman(successor) and self.getScaredTimer(successor) > 0 else 0.0
+        
+        features['enemyPacmanNearMe'] = 0.0
+        minOppDist = 10000
+        minOppPos = (0, 0)
+        for ep in self.getOpponentPositions(successor):
+            # For a feature later on
+            if ep is not None and self.getMazeDistance(ep, position) < minOppDist:
+                minOppDist = self.getMazeDistance(ep, position)
+                minOppPos = ep
+            if ep is not None and self.getMazeDistance(ep, position) <= 1 and self.isPositionInTeamTerritory(successor, ep):
+                features['enemyPacmanNearMe'] = 1.0
+                
+        features['numSameFriends'] = 0
+        for friend in self.getTeam(successor):
+            if successor.getAgentState(self.index).isPacman is self.isPacman(successor):
+                features['numSameFriends'] = features['numSameFriends'] + 1
+
+        # Compute distance to the nearest food
+        foodList = self.getFood(successor).asList()
+        if len(foodList) > 0: # This should always be True,    but better safe than sorry
+            minDiffDistance = min([1000] + [self.getMazeDistance(position, food) - self.getMazeDistance(minOppPos, food) for food in foodList if minOppDist < 1000])
+            features['blockableFood'] = 1.0 if minDiffDistance < 1.0 else 0.0
+
+        return features
