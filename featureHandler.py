b5195a5fa9d7d55ce57d9cc91cefb30fd0f0d16a
Alex Eckert <aeckert@stanford.edu>
2011-11-29 23:40:50 -0800

    updated the feature handler

--- featureHandler.py
+++ featureHandler.py
@@ -1,12 +1,35 @@
-import sys
+import ast
+import AgentEnums
 
-
-class FeatureWriter:
+class FeatureHandler:
     
-    def __init__(self, agentName):
-        self.agentName = agentName
     
-    def writeFeatureVector(self, features):
-        f = open(self.agentName + '.py' , 'w')
-        f.write(self.agentName + 'Weights = ' + str(features))
+    def readAllFeatureWeights(self):
+        f = open('FeatureWeights.py', 'r')
+        for line in f:
+            partitionedLine = line.partition(' = ')
+            name = partitionedLine[0]
+            features = ast.literal_eval(partitionedLine[2])
+            self.allFeatureVectors[name] = features
+        
+        f.close()
+            
+    
+    def __init__(self):
+        self.allFeatureVectors = {}
+        self.agentType = AgentEnums(agentType)
+        self.readAllFeatureWeights()
+    
+    def updateFeatureWeights(self, features):
+        self.allFeatureVectors[str(self.agentType)] = features
+        self.writeFeatureVector()
+    
+    def getFeatureWeights(self, agentType):
+        return self.allFeatureVectors[self.agentName]
+    
+    def writeFeatureVector(self):
+        
+        f = open('FeatureWeights.py' , 'w')
+        for name, features in self.allFeatureVectors.items():
+            f.write(name + ' = ' + str(features))
         f.close()
