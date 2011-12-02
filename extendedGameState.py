3d36808509866ab85f8c1117b01c8916bc01248b
Alex Eckert <aeckert@stanford.edu>
2011-12-01 21:57:39 -0800

    added a wrapper for gamestate to keep track of opponent positions too. Going to use this for minimax

---
+++ extendedGameState.py
@@ -0,0 +1,47 @@
+import sys
+import game
+
+class ExtendedGameState(GameState):
+
+    def __init__(self, state = None):
+        GameState.__init__(self, state)
+        self.positions = []
+
+        for i in range(self.getNumAgents()):
+            self.positions[i] = GameState.getAgentPosition(self, i) if state != None else state.positions[i]
+
+    def getAgentPosition(self, index):
+        actualPos = GameState.getAgentPosition(self, index)
+        if actualPos != None:
+            return actualPos
+
+        return self.positions[index]
+
+    
+    def setBelievedPositionForAgent(self, index, position):
+        self.positions[index] = position
+
+    
+    def agentIsObservable(self, index):
+        return GameState.getAgentPosition(index) != None
+            
+    
+    def generateSuccessor(self, agentIndex, action):
+        if self.agentIsObservable(agentIndex):
+            return ExtendedGameState(GameState.generateSuccessor(self, agentIndex, action))
+        
+        nextPosForAgent = Actions.getSuccessor(self.positions[i], action)
+                
+        if self.hasWall(nextPosForAgent[0], nextPosForAgent[1]):
+            return self
+        
+        nextState = ExtendedGameState(self)
+        nextState.positions[agentIndex] = nextPosForAgent
+        return nextState
+
+    
+        
+
+        
+
+        
